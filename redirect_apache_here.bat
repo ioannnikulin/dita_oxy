04bd650d671b528f25d96a99c7c5cb93283f00a2
ioann <ioann.nikulin@gmail.com>
2022-07-31 14:18:22 +0300

    clean htdocs before symlinking

--- redirect_apache_here.bat
+++ redirect_apache_here.bat
@@ -1,1 +1,2 @@
-mklink /D "D:\xampp\htdocs\dita" "%~dp0demos\flowers\bare tabs without dita"
+rmdir /s /q "D:\xampp\htdocs\dita"
+mklink /D "D:\xampp\htdocs\dita" "%~dp0demos\flowers\out\webhelp-responsive"
